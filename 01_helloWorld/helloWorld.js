679d2d679a1736f5ae8b57d06620b4a22092b176
Mr-Clumsee <105485237+Mr-Clumsee@users.noreply.github.com>
2022-07-01 21:21:15 +0530

    Update helloWorld.js

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,5 +1,6 @@
 const helloWorld = function() {
-  return ''
+  // return was empty, that's why it was showing no result.
+  return 'Hi! Now I am not empty. I am a string.'
 };
 
 module.exports = helloWorld;
