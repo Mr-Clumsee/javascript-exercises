00b12d2263757d52fa428258978264ce521d1f99
Mr-Clumsee <105485237+Mr-Clumsee@users.noreply.github.com>
2022-07-02 11:51:09 +0530

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+   if (num<0) return 'ERROR';
 
+    // using repeat() method.
+    let result = string.repeat(num);
+    return result;
 };
 
 // Do not edit below this line
